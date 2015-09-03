0c0fa4515275dde8e52034c90baa3552a08e5a58
enjelika <enjelika@yahoo.com>
2015-09-02 21:16:51 -0500

    Added Screen Orientation as Portrait - Debra

--- app/src/main/java/edu/uco/robotics/fall15/nxtbluetoothcontroller/MainActivity.java
+++ app/src/main/java/edu/uco/robotics/fall15/nxtbluetoothcontroller/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.uco.robotics.fall15.nxtbluetoothcontroller;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         //Initialize all Image Buttons
         btnUp = (ImageButton) findViewById(R.id.btnup);
